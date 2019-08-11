19e8d5bc4dcc279354ef5374a154913cda063f50
oephi <r.sickle@protonmail.com>
2019-08-11 16:33:54 +1000

    Create user table constraints

--- lib/medium_graphql_api/accounts/user.ex
+++ lib/medium_graphql_api/accounts/user.ex
@@ -3,19 +3,42 @@ defmodule MediumGraphqlApi.Accounts.User do
   import Ecto.Changeset
 
   schema "users" do
-    field(:email, :string)
+    field(:email, :string, unique: true)
     field(:first_name, :string)
     field(:last_name, :string)
     field(:password_hash, :string)
-    field(:role, :string)
+    # virtual means that the field is not included in the db schema
+    field(:password, :string, virtual: true)
+    field(:password_confirmation, :string, virtual: true)
+    field(:role, :string, default: "user")
 
     timestamps()
   end
 
   @doc false
+  # take user and given attributes and check is the atrributes match what is on the user.  If they do, then validate each field to make sure they exist
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:first_name, :last_name, :email, :password_hash, :role])
-    |> validate_required([:first_name, :last_name, :email, :password_hash, :role])
+    # cast puts the attrs into the user struct
+    # validate checks for the all the fields that a user has to give
+    |> cast(attrs, [:first_name, :last_name, :email, :password, :password_confirmation, :role])
+    |> validate_required([
+      :first_name,
+      :last_name,
+      :email,
+      :password,
+      :password_confirmation,
+      :role
+    ])
+    |> validate_format(:email, ~r/@/)
+    |> update_change(:email, &String.downcase(&1))
+    |> validate_length(:password, min: 6, max: 100)
+    |> validate_confirmation(:password)
+    |> unique_constraint(:email)
+    |> hash_password
+  end
+
+  defp hash_password(changeset) do
+    changeset
   end
 end
