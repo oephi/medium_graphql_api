05594d0900832c328ec5508b5e250e64a15b2737
oephi <r.sickle@protonmail.com>
2019-08-11 18:37:48 +1000

    Setup schema file

---
+++ lib/medium_graphql_api_web/schema.ex
@@ -0,0 +1,18 @@
+defmodule MediumGraphqlApiWeb.Schema do
+  use Absinthe.Schema
+
+  # import Types.  The data structures you want to load in your Api.
+
+  query do
+    
+  end
+
+  # mutation do
+
+  # end
+
+  # subscription do
+
+  # end
+
+end
