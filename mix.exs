7d0bb856916026105d67ad3509ffe8bffd5f3b84
oephi <r.sickle@protonmail.com>
2019-08-11 17:24:38 +1000

    Add password hash function

--- mix.exs
+++ mix.exs
@@ -38,7 +38,8 @@ defmodule MediumGraphqlApi.Mixfile do
       {:phoenix_ecto, "~> 3.2"},
       {:postgrex, ">= 0.0.0"},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:argon2_elixir, "~> 2.0"}
     ]
   end
 
